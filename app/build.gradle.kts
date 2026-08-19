f761b8876ec32b22f1154484a3a640a97f1ef035
ua0674085656-ops <ua0674085656@gmail.com>
2026-08-19 23:08:05 +0300

    Add storage access dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,5 +30,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.documentfile:documentfile:1.1.0")
     implementation("com.google.android.material:material:1.13.0")
 }
